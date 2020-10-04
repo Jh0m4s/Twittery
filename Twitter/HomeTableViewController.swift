1a0af11a63e84b9a2fc3e73f1f0196a561b31861
Joey Thomas <joeythomas@Joeys-iMac.lan>
2020-10-03 23:00:25 -0700

    Can now tweet from app- save point before I go onto like/retweeting

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -23,6 +23,11 @@ class HomeTableViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        loadTweet()
+    }
 
     // MARK: - Table view data source
     
